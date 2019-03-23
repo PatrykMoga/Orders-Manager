7aa89208cc6a73059aa5008211f45b3a0111a74a
Patryk Moga <46868740+PatrykMoga@users.noreply.github.com>
2019-03-23 09:55:29 +0100

    commit

---
+++ OrdersManager.Core/Deserializers/JsonDeserializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using OrdersManager.Core.Mapping;
+using OrdersManager.Core.Requests;
+
+namespace OrdersManager.Core.Deserializers
+{
+    public class JsonDeserializer : IDeserializer
+    {
+        public IList<IRequest> DeserializeFiles(IEnumerable<string> files)
+        {
+            var requests = new List<IRequest>();
+
+            foreach (var file in files.Where(f => f.EndsWith(".json")))
+            {
+                requests.AddRange(DeserializeFile(file));
+            }
+            return requests;
+        }
+
+        public IList<IRequest> DeserializeFile(string file)
+        {
+            return (IList<IRequest>)JsonConvert.DeserializeObject<List<RequestJson>>(file);
+        }
+    }
+}
---
+++ OrdersManager.Core/Mapping/RequestJson.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using OrdersManager.Core.Requests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrdersManager.Core.Mapping
+{
+    public class RequestJson : IRequest
+    {
+        [JsonProperty("clientId")]
+        public string ClientId { get; set; }
+        [JsonProperty("requestId")]
+        public long RequestId { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("quantity")]
+        public int Quantity { get; set; }
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+    }
+}
